24dca05d996545362432b2a536816cc4e84bb066
niteshkhnduja19943 <niteshkhanduja19943@gmail.com>
2018-04-26 15:31:23 +0530

    `Register login and update profile`

---
+++ users/serializers.py
@@ -0,0 +1,139 @@
+from rest_framework import serializers
+
+from .models import User
+from django.contrib.auth import authenticate
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the client.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    pin = serializers.IntegerField()
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+        fields = ['username', 'pin', 'password', 'token']
+
+    def create(self, validated_data):
+        # Use the `create_user` method we wrote earlier to create a new user.
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255, write_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    pin = serializers.IntegerField(write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        # The `validate` method is where we make sure that the current
+        # instance of `LoginSerializer` has "valid". In the case of logging a
+        # user in, this means validating that they've provided an email
+        # and password and that this combination matches one of the users in
+        # our database.
+        username = data.get('username', None)
+        pin = data.get('pin', None)
+        password = data.get('password', None)
+
+        # Raise an exception if an
+        # pin is not provided.
+        if pin is None:
+            raise serializers.ValidationError(
+                'An pin address is required to log in.'
+            )
+
+        # Raise an exception if a
+        # password is not provided.
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+
+        user = authenticate(username=username, pin=pin, password=password)
+
+        # If no user was found matching this pin/password combination then
+        # `authenticate` will return `None`. Raise an exception in this case.
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+
+        # Django provides a flag on our `User` model called `is_active`. The
+        # purpose of this flag is to tell us whether the user has been banned
+        # or deactivated. This will almost never be the case, but
+        # it is worth checking. Raise an exception in this case.
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+
+        # The `validate` method should return a dictionary of validated data.
+        # This is the data that is passed to the `create` and `update` methods
+        # that we will see later on.
+        return {
+            'pin': user.pin,
+            'username': user.username,
+            'token': user.token
+        }
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Handles serialization and deserialization of User objects."""
+
+    # Passwords must be at least 8 characters, but no more than 128
+    # pin must be updated too
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+    pin = serializers.IntegerField()
+
+    class Meta:
+        model = User
+        fields = ('pin', 'username', 'password', 'token',)
+
+        read_only_fields = ('token',)
+
+    def update(self, instance, validated_data):
+        """Performs an update on a User."""
+
+        # Passwords should not be handled with `setattr`, unlike other fields.
+        # Django provides a function that handles hashing and
+        # salting passwords. That means
+        # we need to remove the password field from the
+        # `validated_data` dictionary before iterating over it.
+        password = validated_data.pop('password', None)
+        pin = validated_data.pop('pin', None)
+
+        for (key, value) in validated_data.items():
+            # For the keys remaining in `validated_data`, we will set them on
+            # the current `User` instance one at a time.
+            setattr(instance, key, value)
+
+        if password is not None:
+            # `.set_password()`  handles all
+            # of the security stuff that we shouldn't be concerned with.
+            instance.set_password(password)
+        if pin is not None:
+            instance.save()
+
+        # After everything has been updated we must explicitly save
+        # the model. It's worth pointing out that `.set_password()` does not
+        # save the model.
+        instance.save()
+
+        return instance
